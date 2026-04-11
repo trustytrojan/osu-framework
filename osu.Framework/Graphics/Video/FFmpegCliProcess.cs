d849b1e7797db22467f69672a80e58e7ed256760
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2026-04-11 14:37:41 -0400

    audio recording works!!!!

--- osu.Framework/Graphics/Video/FFmpegCliProcess.cs
+++ osu.Framework/Graphics/Video/FFmpegCliProcess.cs
@@ -3,7 +3,10 @@
 
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using osu.Framework.Extensions.ImageExtensions;
 using osu.Framework.Logging;
 using osuTK;
@@ -16,6 +19,7 @@ namespace osu.Framework.Graphics.Video
     {
         private Process ffmpegProcess;
         private readonly Vector2 videoSize;
+        private readonly NamedPipeServerStream? audioPipe;
 
         public FFmpegCliProcess(string outputFilePath, Vector2 videoSize, int framerate, string videoCodec = "libx264")
         {
@@ -53,6 +57,56 @@ namespace osu.Framework.Graphics.Video
             ffmpegProcess.Start();
         }
 
+        public FFmpegCliProcess(string outputFilePath, Vector2 videoSize, int framerate, int audioSampleRate, string audioSampleFormat, int audioChannels, string videoCodec = "libx264")
+        {
+            this.videoSize = videoSize;
+
+            string audioPipeName = $"osu-framework-ffmpeg-audio-{Guid.NewGuid():N}";
+            audioPipe = new NamedPipeServerStream(audioPipeName, PipeDirection.Out, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+
+            ffmpegProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $"-hide_banner -hwaccel auto -y -f rawvideo -pix_fmt rgba -s {(int)videoSize.X}x{(int)videoSize.Y} -r {framerate} -i - -f {audioSampleFormat} -ar {audioSampleRate} -ac {audioChannels} -i \"{toNamedPipePath(audioPipeName)}\" -map 0:v -map 1:a -c:v {videoCodec} -c:a copy -shortest \"{outputFilePath}\"",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            Logger.Log($"ffmpegProcess.StartInfo.Arguments: {ffmpegProcess.StartInfo.Arguments}");
+        }
+
+        public async Task Start()
+        {
+            var connectTask = audioPipe?.WaitForConnectionAsync();
+
+            ffmpegProcess.Exited += (_, _) =>
+            {
+                if (ffmpegProcess.ExitCode != 0)
+                    throw new IOException($"ffmpeg exited with {ffmpegProcess.ExitCode}");
+            };
+            ffmpegProcess.Start();
+
+            if (connectTask == null)
+                return;
+
+            // 3. Now wait for the connection to actually be established.
+            // Set a timeout so you don't hang forever if FFmpeg fails.
+            if (await Task.WhenAny(connectTask, Task.Delay(5000)) == connectTask)
+            {
+                // Connection successful
+                await connectTask;
+            }
+            else
+            {
+                // FFmpeg likely failed to connect or crashed
+                throw new TimeoutException($"FFmpeg failed to connect to the audio pipe. Exit Code: {(ffmpegProcess.HasExited ? ffmpegProcess.ExitCode : "Still running")}");
+            }
+        }
+
         public void WriteFrame(Image<Rgba32> image)
         {
             if (image.Size.Width != videoSize.X || image.Size.Height != videoSize.Y)
@@ -65,9 +119,33 @@ namespace osu.Framework.Graphics.Video
             stream.Write(rgbaBytes);
         }
 
-        public void Close()
+        public void WriteAudio(ReadOnlySpan<byte> audioData)
         {
-            ffmpegProcess.Close();
+            if (audioPipe == null || !audioPipe.IsConnected)
+                return;
+
+            audioPipe.Write(audioData);
+        }
+
+        public void Dispose()
+        {
+            audioPipe?.Dispose();
+            ffmpegProcess.Dispose();
+        }
+
+        private string toNamedPipePath(string pipeName)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return $@"\\.\pipe\{pipeName}";
+            }
+            else
+            {
+                // On Linux/Mac, .NET creates a socket file in /tmp/
+                // FFmpeg needs the 'unix' protocol prefix to connect to a socket
+                string socketPath = Path.Combine(Path.GetTempPath(), $"CoreFxPipe_{pipeName}");
+                return $"unix://{socketPath}";
+            }
         }
     }
 }
